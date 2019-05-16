24f61c9d22f2d0fb419df2a3c588bdd210115305
Vishal Thakur <vishalthakur301999@gmail.com>
2019-05-16 15:02:04 +0530

    update to index.php

--- index.php
+++ index.php
@@ -28,8 +28,7 @@ if($method == 'POST'){
     }
 
     $response = new \stdClass();
-    $response->speech = $speech;
-    $response->displayText = $speech;
+    $response->fulfillmentText = $speech;
     $response->source = "webhook";
     echo json_encode($response);
 }
