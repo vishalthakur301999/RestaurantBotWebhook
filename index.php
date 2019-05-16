50984b63c476027c855192c5c777ad9588ff24b9
Vishal Thakur <vishalthakur301999@gmail.com>
2019-05-16 15:49:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+$servername = "sql12.freemysqlhosting.net";
+$username = "sql12291966";
+$password = "aJehBgyhb2";
+$db = "sql12291966";
+$conn = mysqli_connect($servername,$username,$password);
+mysqli_select_db($conn,$db);
 
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -9,7 +15,8 @@ if($method == 'POST'){
     $date_time = $json->queryResult->outputContexts[0]->parameters->date_time->date_time;
     $number = $json->queryResult->outputContexts[0]->parameters->number;
     $speech = "Reservation done for ".$number." people on ".$date_time;
-
+    $query = "insert into Bookings(NoM,DateTime) values ($number,$date_time);";
+    mysqli_query($conn,$query);
     $response = new \stdClass();
     $response->fulfillmentText = $speech;
     $response->source = "webhook";
